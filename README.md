05314f18f387964f8a5284f8e5d3ab6ee184299b
ManivannanE <manivannanelangovan@syncfusion.com>
2018-12-18 15:07:37 +0530

    Sample committed for series data template

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # how-to-customize-the-chart-series-in-wpf-sfchart
 This example demonstrates how to customize the chart series in WPF platform using the data template.
+
+For more details see [customize series](https://help.syncfusion.com/wpf/sfchart/styling-and-customization#customize-series) WPF user guide.
+
+## <a name="requirements-to-run-the-demo"></a>Requirements to run the demo ##
+
+* [Visual Studio 2017](https://visualstudio.microsoft.com/downloads/)
+
+## <a name="troubleshooting"></a>Troubleshooting ##
+### Path too long exception
+If you are facing path too long exception when building this example project, close Visual Studio and rename the repository to short and build the project.
+
+## <a name="output"></a>Output ##
+
+![output](Series_DataTemplate.png)
---
+++ SeriesCustomTemplate/SeriesCustomTemplate/ColorConverter.cs
@@ -0,0 +1,176 @@
+﻿using Syncfusion.UI.Xaml.Charts;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SeriesCustomTemplate
+{
+    public class BubbleInteriorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var segment = value as ChartSegment;
+
+            if (segment != null)
+            {
+                if ((segment.Item as Model).Size > 30)
+                {
+                    return new SolidColorBrush(Colors.Red);
+                }
+                else
+                {
+                    return new SolidColorBrush(Colors.Green);
+                }
+
+            }
+
+            return new SolidColorBrush(Colors.Green);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BubbleAngleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var segment = value as BubbleSegment;
+
+            if (segment != null)
+            {
+                if ((segment.Item as Model).Count > 20)
+                {
+                    return 180;
+                }
+                else
+                {
+                    return 360;
+                }
+            }
+
+            return 360;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class StepLineInteriorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var segment = value as StepLineSegment;
+
+            if (segment.YData > 25)
+                return new SolidColorBrush(Colors.Red);
+            else
+                return new SolidColorBrush(Colors.Blue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+
+    public class StepLineDashesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var segment = value as StepLineSegment;
+
+            if (segment.YData > 25)
+                return new DoubleCollection { 2, 3 };
+            else
+                return new DoubleCollection { 2, 0 };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+
+    public class ScatterInteriorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var segment = value as ChartSegment;
+
+            if (segment != null)
+            {
+                if ((segment.Item as Model).Count > 20)
+                {
+                    return new SolidColorBrush(Colors.Black);
+                }
+                else
+                {
+                    return new SolidColorBrush(Colors.Gray);
+                }
+
+            }
+
+            return new SolidColorBrush(Colors.Gray);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ScatterAngleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var segment = value as BubbleSegment;
+
+            if (segment != null)
+            {
+                if ((segment.Item as Model).Count > 20)
+                {
+                    return 180;
+                }
+                else
+                {
+                    return 360;
+                }
+
+            }
+
+            return 360;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SplineColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ((value as Syncfusion.UI.Xaml.Charts.LineSegment).YData > 20)
+                return new SolidColorBrush(Colors.Red);
+            else
+                return new SolidColorBrush(Colors.Blue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
